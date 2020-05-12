068723277d8302eb71b47387cd1a689d1ecf9f61
2422527665@qq.com <2422527665@qq.com>
2020-05-12 16:58:42 +0800

    feat: mint-ui

---
+++ src/components/public/footer.vue
@@ -0,0 +1,36 @@
+<template>
+<div id="foot">
+    <mt-tabbar fixe="true" value="" v-model="selected">
+        <mt-tab-item id="首页">
+            <img slot="icon" src="../../assets/logo.png">
+            首页
+        </mt-tab-item>
+        <mt-tab-item id="礼物">
+            <img slot="icon" src="../../assets/logo.png">
+            礼物
+        </mt-tab-item>
+        <mt-tab-item id="计划">
+            <img slot="icon" src="../../assets/logo.png">
+            计划
+        </mt-tab-item>
+        <mt-tab-item id="发现">
+            <img slot="icon" src="../../assets/logo.png">
+            发现
+     </mt-tab-item>
+</mt-tabbar>
+</div>
+</template>
+<script>
+export default {
+  name: "vFooter",
+  data() {
+    return {
+      selected: "首页"
+    };
+  }
+
+}
+</script>
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/public/header.vue
@@ -0,0 +1,18 @@
+<template>
+<div id="header">
+    <mt-header fixed title="固定在顶部">
+        <router-link to="/" slot="left">
+        <mt-button icon="back">返回</mt-button>
+        </router-link>
+        <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
+</div>
+</template>
+<script>
+export default {
+  name: "vHeader"
+}
+</script>
+<style scoped lang="scss">
+
+</style>
